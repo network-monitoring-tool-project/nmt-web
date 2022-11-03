74afe7ff60b1e8b31feab854130dc7b7eaeba233
f0xcb <f0xcb@mailbox.org>
2022-11-03 16:59:03 +0100

    [-] removed spati permissions [+] default admin account [+] add package-lock.json to .gitignore

--- application/database/seeders/DatabaseSeeder.php
+++ application/database/seeders/DatabaseSeeder.php
@@ -15,7 +15,7 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->call([
-            RoleAndPermissionSeeder::class
+            UserSeeder::class
         ]);
     }
 }
---
+++ application/database/seeders/UserSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run(): void
+    {
+        DB::table('users')->insert([
+            'name' => 'superadmin',
+            'email' => 'admin@nmt.local',
+            'password' => Hash::make('superadmin123')
+        ]);
+    }
+}
