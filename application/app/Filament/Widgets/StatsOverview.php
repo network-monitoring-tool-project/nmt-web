d32904806fa4f24f87188b2217218b1e3d3dd6d3
f0xcb <f0xcb@mailbox.org>
2022-11-09 20:19:38 +0100

    [+] Adding new card on dashboard

--- application/app/Filament/Widgets/StatsOverview.php
+++ application/app/Filament/Widgets/StatsOverview.php
@@ -2,6 +2,7 @@
 
 namespace App\Filament\Widgets;
 
+use App\Models\Address;
 use App\Models\Scan;
 use App\Models\User;
 use Filament\Widgets\StatsOverviewWidget as BaseWidget;
@@ -12,8 +13,9 @@ class StatsOverview extends BaseWidget
     protected function getCards(): array
     {
         return [
-            Card::make('User', count(User::all())),
-            Card::make('Scans', count(Scan::all()))
+            Card::make('Users', count(User::all())),
+            Card::make('Scans', count(Scan::all())),
+            Card::make('Addresses', count(Address::all()))
         ];
     }
 }
